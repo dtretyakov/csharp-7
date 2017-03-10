761f688426d1608e51654040fde3c14ecc9082ce
Dmitry.Tretyakov <dtretyakov@gmail.com>
2017-03-10 11:11:08 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void PrintStars(string s)
+        {
+            if (int.TryParse(s, out var i)) { Console.WriteLine(new string('*', i)); }
+            else { Console.WriteLine("No stars!"); }
+        }
+
+        public void PrintStars2(object o)
+        {
+            if (o is null) return;
+            if (!(o is int i)) return;
+            Console.WriteLine(new string('*', i));
+
+            switch (o)
+            {
+                case int c:
+                    Console.WriteLine($"int {c}");
+                    break;
+                case long s:
+                    Console.WriteLine($"long {s}");
+                    break;
+                default:
+                    Console.WriteLine("default");
+                    break;
+                case null:
+                    throw new ArgumentNullException(nameof(o));
+            }
+
+            var x = 2;
+            if (x < 0) throw new ArgumentException("Не надо негатива!", nameof(x));
+            fun(x);
+
+            int fun(int z)
+            {
+                return z + 1;
+            }
+        }
+    }
+}
